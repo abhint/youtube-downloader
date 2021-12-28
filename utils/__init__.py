e84adf688877830052796ed3e153d310e6de27ae
AbhijithNT <abhijithnt64@gmail.com>
2021-12-28 19:50:23 +0530

    init

---
+++ utils/__init__.py
@@ -0,0 +1,3 @@
+from .tool import get_youtube_extract
+
+REGEX = r'(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|playlist\?|watch\?v=|watch\?.+(?:&|&#38;);v=))([a-zA-Z0-9\-_]{11})?(?:(?:\?|&|&#38;)index=((?:\d){1,3}))?(?:(?:\?|&|&#38;)?list=([a-zA-Z\-_0-9]{34}))?(?:\S+)?'
